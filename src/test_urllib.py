c0a4afd62f03d84843d5c1a8b20ea41724a45cb8
tanfengkui <tanfengkui@hotmail.com>
2017-01-31 21:54:56 +0800

    try encoding

--- src/test_urllib.py
+++ src/test_urllib.py
@@ -1,12 +1,18 @@
 import urllib.request
 import urllib
 
-# notice, that urllib is modified in Python 3, the urlopen is removed to sub-mobule request, also you need import this
-# module
+# notice 1 :urllib is move to sub-module request
+# notice 2:the url to retrive data is changed, the noaa web site is rebuild.
 
-f = urllib.request.urlopen('http://www.baidu.com')
+#response  = urllib.request.urlopen('http://services.swpc.noaa.gov/text/predicted-sunspot-radio-flux.txt')
+response  = urllib.request.urlopen('http://www.bsb.com.cn/')
 
-d = f.read ()
+print('http header:\n',response .info())
+print('http statues:',response .getcode())
+print('url:',response .geturl())
 
 
-print (d)
+for line in response.readlines():
+    print(bytes.decode(line,"UTF-8"))
+
+response.close()
